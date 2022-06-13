acd6ababe4cb2aff97311b5b78e16a8cc2f2df25
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-06-13 22:44:16 +0900

    www
    wwww

---
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+// 비용과 날짜를 화면에 렌더링하는 컴포넌트
+import './ExpenseDate.css';
+
+function ExpenseDate(props){
+    const month = props.date.toLocaleString('en-US',{month:'long'});
+    const day = props.date.toLocaleString('en-US',{day:'2-digit'});
+    const year = props.date.getFullYear();
+    return (
+        <div className="expense-date">
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
+            <div className="expense-date__day">{day}</div>
+        </div>
+    )
+}
+
+export default ExpenseDate;
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,6 +1,18 @@
-function ExpenseItem(){
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate.js';
+import Card from './Card.js';
+
+function ExpenseItem(props){ //app.js에서 받아온변수들을 매개변수로 사용
+
+    // props는 app.js에서 전달한 data들의 집합이다.
     return(
-        <h2>Expense item!</h2>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
     )
 }
 
