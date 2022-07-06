69893dad14fda5aee1f57779c3927615bb8ad780
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-07-07 02:46:27 +0900

    SDF
    SDF

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -51,25 +51,28 @@ function ExpenseForm() {
         };
 
         console.log(expenseData);
+        setEnteredAmount("");
+        setEnteredDate("");
+        setEnteredTitle("");
     }
   return (
     <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
         </div>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" placeholder='Amount' onChange={amountChangeHandler} />
+                <input type="number" value={enteredAmount} min="0.01" step="0.01" placeholder='Amount' onChange={amountChangeHandler} />
             </div>
         </div>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type="Date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                <input type="Date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className='new-expense__actions'>
