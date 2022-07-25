c27f25c8ee044ea92299f331aa4d471c3d284e2b
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-07-26 03:11:04 +0900

    cleanupFunction
    0726

--- 02-useEffect/src/components/Login/Login.js
+++ 02-useEffect/src/components/Login/Login.js
@@ -12,15 +12,28 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=>{
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      //boolean && boolean
-    );
-  },[setFormIsValid,enteredEmail,enteredPassword])
+    const identifier = setTimeout(()=>{
+      console.log("checking");
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+        //boolean && boolean
+      );
+    },500);
+    return ()=>{
+      console.log('cleanup');
+      clearTimeout(identifier);
+    }; 
+    //cleanup function
+    //1.useEffect로 선언한 sideEffect function이 실행될 떄 실행(첫 번째 sideEffect실행은 실행x)
+    //2.특정 컴포넌트가 재사용될 때마다 실행
+    //클린업 함수에 clearTimeout를 넣어서 클린업함수가 실행될때마다 타이머 지움
+    //즉, 새로운 타이머를 설정하기 전에 마지막 타이머를 지움
+    //사용자가 타이핑을 멈춘 이후 0.5초 뒤에 sideEffect함수를 실행하는것
+    //계속 타이핑 하고있으면 cleanup함수에 의해 타이머가지워짐
+  },[enteredEmail,enteredPassword])
   //의존성 배열에 함수의 실행을 추가하지말고 포인터를 추가
   //함수의 실행을 추가하면 함수의 결과값이 의존성에 들어감
   //여기 3개의 의존성의 컴포넌트 렌더링 주기에 변경된 경우만 setFormIsValid를 실행
-  //셋중 하나라도 바뀐게 없으면 실행x
 
 
   //무언가에 대한 응답(sideEffect)으로 실행되는 코드에 useEffect는 굉장한 도움이 됨
