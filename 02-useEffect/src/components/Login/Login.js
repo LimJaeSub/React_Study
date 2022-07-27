04a96370018807ba9a04550e0e1928a7fd0bb0df
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-07-28 01:16:49 +0900

    useReducer
    0728

--- 02-useEffect/src/components/Login/Login.js
+++ 02-useEffect/src/components/Login/Login.js
@@ -1,53 +1,77 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState,useEffect,useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer=(state,action)=>{
+  if(action.type==='USER_INPUT'){
+    return {value:action.val, isValid:action.val.includes('@')}; //action의 type이 USER_INPUT일 경우 value와 isValid의 상태를 업데이트 한다.
+  }
+  if(action.type==='INPUT_BLUR'){
+    return{value:state.value,isValid:state.value.includes('@')};//value에 현재 상태인 state도 사용이 가능
+  }
+  return {value:'',isValid:false};
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+
+  const [emailState,dispatchEmail] = useReducer(emailReducer,{value:'',isValid:null});
+
   useEffect(()=>{
-    const identifier = setTimeout(()=>{
-      console.log("checking");
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-        //boolean && boolean
-      );
-    },500);
-    return ()=>{
-      console.log('cleanup');
-      clearTimeout(identifier);
-    }; 
-    //cleanup function
-    //1.useEffect로 선언한 sideEffect function이 실행될 떄 실행(첫 번째 sideEffect실행은 실행x)
-    //2.특정 컴포넌트가 재사용될 때마다 실행
-    //클린업 함수에 clearTimeout를 넣어서 클린업함수가 실행될때마다 타이머 지움
-    //즉, 새로운 타이머를 설정하기 전에 마지막 타이머를 지움
-    //사용자가 타이핑을 멈춘 이후 0.5초 뒤에 sideEffect함수를 실행하는것
-    //계속 타이핑 하고있으면 cleanup함수에 의해 타이머가지워짐
-  },[enteredEmail,enteredPassword])
-  //의존성 배열에 함수의 실행을 추가하지말고 포인터를 추가
-  //함수의 실행을 추가하면 함수의 결과값이 의존성에 들어감
-  //여기 3개의 의존성의 컴포넌트 렌더링 주기에 변경된 경우만 setFormIsValid를 실행
+    console.log('Effect Running');
+  });
+
+  // useEffect(()=>{
+  //   const identifier = setTimeout(()=>{
+  //     console.log("checking");
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //       //boolean && boolean
+  //     );
+  //   },500);
+  //   return ()=>{
+  //     console.log('cleanup');
+  //     clearTimeout(identifier);
+  //   }; 
+  //   //cleanup function
+  //   //1.useEffect로 선언한 sideEffect function이 실행될 떄 실행(첫 번째 sideEffect실행은 실행x)
+  //   //2.특정 컴포넌트가 재사용될 때마다 실행
+  //   //클린업 함수에 clearTimeout를 넣어서 클린업함수가 실행될때마다 타이머 지움
+  //   //즉, 새로운 타이머를 설정하기 전에 마지막 타이머를 지움
+  //   //사용자가 타이핑을 멈춘 이후 0.5초 뒤에 sideEffect함수를 실행하는것
+  //   //계속 타이핑 하고있으면 cleanup함수에 의해 타이머가지워짐
+  // },[enteredEmail,enteredPassword])
+  // //의존성 배열에 함수의 실행을 추가하지말고 포인터를 추가
+  // //함수의 실행을 추가하면 함수의 결과값이 의존성에 들어감
+  // //여기 3개의 의존성의 컴포넌트 렌더링 주기에 변경된 경우만 setFormIsValid를 실행
 
 
   //무언가에 대한 응답(sideEffect)으로 실행되는 코드에 useEffect는 굉장한 도움이 됨
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type:'UESR_INPUT',val:event.target.value});
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length>6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length>6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({type:'INPUT_BLUR'});
   };
 
   const validatePasswordHandler = () => {
@@ -56,7 +80,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -64,14 +88,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
