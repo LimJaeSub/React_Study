9ab59016749ac82a94dbbe91dafada16c7427130
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-10-05 22:28:38 +0900

    useEffect & useReducer fin
    1005

--- 02_useEffect_useReducer/src/components/Login/Login.js
+++ 02_useEffect_useReducer/src/components/Login/Login.js
@@ -14,46 +14,59 @@ const emailReducer = (state,action)=>{
   return {value:'',isValid:false};
   // default state
 }
+
+const passwordReducer = (state,action)=>{
+  switch(action.type){
+    case 'USER_INPUT':
+      return {value:action.val,isValid:action.val.trim().length>6};
+    case 'INPUT_BLUR':
+      return {value:state.value,isValid:state.value.trim().length>6};
+    default:
+      return {value:'',isValid:false}
+  }
+}
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
-
-
   const [emailState,dispatchEmail] = useReducer(emailReducer,{value:'',isValid:null});
+  const [passwordState,dispatchPassword]=useReducer(passwordReducer,{value:'',isValid:null});
 
-  // useEffect(()=>{
-  //   const identifier = setTimeout(()=>{
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   },500);
+  const {isValid:emailIsValid} = emailState;
+  const {isValid:passwordIsValid} = passwordState;
+
+  useEffect(()=>{
+    const identifier = setTimeout(()=>{
+      setFormIsValid(
+        emailIsValid && passwordIsValid
+      );
+    },500);
     
-  //   return ()=>{
-  //     console.log("cleanup!");
-  //     clearTimeout(identifier);
-  //   };
-  //   //처음 실행되는 경우를 제외하고 위 useEffect함수가 실행된 후 cleanup함수 실행
-  //   //컴포넌트가 리렌더링이나 사라진 후 cleanup함수를 실행한다.
-  // },[enteredEmail,enteredPassword]);
-  // //둘 중 하나라도 바뀐게 있으면 실행
+    return ()=>{
+      console.log("cleanup!");
+      clearTimeout(identifier);
+    };
+    //처음 실행되는 경우를 제외하고 위 useEffect함수가 실행된 후 cleanup함수 실행
+    //컴포넌트가 리렌더링이나 사라진 후 cleanup함수를 실행한다.
+  },[emailIsValid,passwordIsValid]);
+  //둘 중 하나라도 바뀐게 있으면 실행
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type:'USER_INPUT',val:event.target.value})
 
-    setFormIsValid(
-      emailState.isValid && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(
+    //   emailState.isValid && passwordState.isValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({type:'USER_INPUT',val:event.target.value})
 
-    setFormIsValid(
-      emailState.isValid && event.target.value.trim().length > 6
-    );
+    // setFormIsValid(
+    //   emailState.isValid && event.target.value.trim().length > 6
+    // );
   };
 
   const validateEmailHandler = () => {
@@ -61,12 +74,12 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({type:'INPUT_BLUR'})
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -88,14 +101,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
