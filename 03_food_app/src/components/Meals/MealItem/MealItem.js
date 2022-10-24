06949e5f1de378ebc5c8dd48a7e8076e4456400a
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-10-24 18:54:26 +0900

    context
    1024

--- 03_food_app/src/components/Meals/MealItem/MealItem.js
+++ 03_food_app/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 function MealItem(props) {
+  const ctx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`; //소수점 이하 두 자리 수까지 렌더링
+
+  const addToCartHandler = (amount) => {
+    ctx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  }; // @ MealItemForm과 contxet를 연결하는 매개체
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +22,7 @@ function MealItem(props) {
         <div className={classes.price}>{props.price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- 03_food_app/src/store/CartProvider.js
+++ 03_food_app/src/store/CartProvider.js
@@ -1,28 +1,57 @@
 // @ Context data관리 및 접근하는 component에게 데이터 제공
 import CartContext from "./cart-context";
+import { useReducer } from "react";
 
-const CartProvider = props=>{
-    const addItemToCartHandler = (item)=>{
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const updatedItems = state.items.concat(action.value);
+      // @ 기존 배열을 변형하지 않고 새 배열 반환
+      // @ 현재 상태의 객체 내부값인 items배열에 dispatch함수로 item값을 얻은 value를 삽입해서 새로운 배열 return
+      const newTotalAmount =
+        state.totalAmount + action.value.price * action.value.amount;
+      return {
+        items: updatedItems,
+        totalAmount: newTotalAmount,
+      };
 
-    };
+    case "REMOVE":
+      return 1;
+    default:
+      break;
+  }
+  return defaultCartState;
+};
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  // @ [state객체,dispatch함수] = useReducer(reducer함수,초기상태)
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", value: item });
+  }; // @ 이 함수가 실행될 때마다 장바구니에 추가해야 할 항목을 얻음
 
-    const removeItemFromCartHandler = (id)=>{
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", value: id });
+  }; // @ 이 함수가 실행될 때마다 장바구니에
 
-    };
-
-
-    const cartContext = {
-        items:[],
-        totalAmount:0,
-        addItem:addItemToCartHandler,
-        removeItem:removeItemFromCartHandler
-    }// @ 해당 객체를 context파일에 value값으로 직접 보내줌
-    return(
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    )
-    // @ 이 context에 접는하는 모든 compoenent를 provider로 감쌈
-}
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  }; // @ 해당 객체를 context파일에 value값으로 직접 보내줌
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+  // @ 이 context에 접는하는 모든 compoenent를 provider로 감쌈
+};
 
 export default CartProvider;
