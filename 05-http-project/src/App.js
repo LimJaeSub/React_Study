0aed92125a5572c9f02790ea81017ab1a5c4afc9
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-11-12 23:26:24 +0900

    1112
    1112

--- 05-http-project/src/App.js
+++ 05-http-project/src/App.js
@@ -1,75 +1,84 @@
-import React, { useState,useEffect,useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoding] = useState(false);
-  const [error,setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-
-  // @useCallback을 사용해 불필요하게 재생성되는 경우 방지
-  const fetchMoviesHandler=useCallback(async()=>
-    {
-      setIsLoding(true);
-      setError(null);
-
-      try{
-        const response = await fetch("https://swapi.dev/api/films/");
-        if(!response.ok){
-          throw new Error('somethin went wrong');
-        }
-        const data = await response.json();
-
-
-        // @ response.ok => 응답이 잘 왔는지 확인하는것
-        // throw new Error => 예외를 발생시키는것, 예외가 발생하면 함수가 중지되고 catch문으로 넘어감
-        // response.ok가 false(무언가 문제가 발생)이면 throw로 예외를 발생시키고 catch문으로 전달한다.
-        const transformedMovies = data.results.map((movieData) => {
-            return {
-              id: movieData.episode_id,
-              title: movieData.title,
-              openingText: movieData.opening_crawl,
-              releaseDate: movieData.release_date,
-            };
-          });
-        setMovies(transformedMovies);
-        setIsLoding(false);
-      } catch(error){
-        setError(error.message);
-        setIsLoding(false); // 오류가 발생하면 로딩은 항상 중단
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-5f427-default-rtdb.firebaseio.com/movies.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
       }
-    
+
+      const data = await response.json();
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
     }
-  ,[]);
-  
-  useEffect(()=>{
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
     fetchMoviesHandler();
-  },[fetchMoviesHandler]) 
+  }, [fetchMoviesHandler]);
 
-  let content = <p>fount no movies</p>
+  async function addMovieHandler(movie) {
+    // @ 데이터 전송, POST 요청
+    const response = await fetch(
+      "https://react-http-5f427-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST", // @ firebase에는 post요청을 보내면 리소스를 만들어줌
+        body: JSON.stringify(movie), // @ javascript 객체를 JSON 형식으로 바꿔주는 JSON.stringify()
+        headers: {
+          "Content-Type": "applicatgion/json",
+        },
+      }
+    );
+    const data = await response.json();
+  }
 
-  if(movies.length>0){
-    // @ movies가 잘 들어왔으면
-    content = <MoviesList movies={movies} />
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
-  if(error){
-    content = <p>{error}</p>
+
+  if (error) {
+    content = <p>{error}</p>;
   }
-  if(isLoading){
-    // @ 로딩중이면
-    content = <p>Loading...</p>
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
